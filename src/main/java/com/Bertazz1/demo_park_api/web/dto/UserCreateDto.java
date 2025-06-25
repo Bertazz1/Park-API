a1a20c4ef413c13dfbb1b5721cafa88316620d9a
Gustavo Bertuzzi de Oliveira <159206426+Bertazz1@users.noreply.github.com>
2025-06-24 22:17:05 -0300

    feat/Validando o objeto UsuarioCreateDto

--- src/main/java/com/Bertazz1/demo_park_api/web/dto/UserCreateDto.java
+++ src/main/java/com/Bertazz1/demo_park_api/web/dto/UserCreateDto.java
@@ -1,11 +1,19 @@
 package com.Bertazz1.demo_park_api.web.dto;
 
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.*;
 
 @Getter @Setter @NoArgsConstructor @AllArgsConstructor @ToString
 public class UserCreateDto {
 
+    @NotBlank(message = "Username cannot be blank")
+    @Email(message = "Invalid email format")
     private String username;
+
+    @NotBlank(message = "Password cannot be blank")
+    @Size(min = 6, max = 6, message = "Password must be exactly 6 characters long")
     private String password;
 }
