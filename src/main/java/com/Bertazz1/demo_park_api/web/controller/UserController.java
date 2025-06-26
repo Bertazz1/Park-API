b447e02cee041fe08b082b73ea933d8a38b5312d
Gustavo Bertuzzi de Oliveira <159206426+Bertazz1@users.noreply.github.com>
2025-06-26 08:40:24 -0300

    feat/Adicionando validacoes de requisicao

--- src/main/java/com/Bertazz1/demo_park_api/web/controller/UserController.java
+++ src/main/java/com/Bertazz1/demo_park_api/web/controller/UserController.java
@@ -6,6 +6,7 @@ import com.Bertazz1.demo_park_api.web.dto.UserCreateDto;
 import com.Bertazz1.demo_park_api.web.dto.UserPasswordDtio;
 import com.Bertazz1.demo_park_api.web.dto.UserResposeDto;
 import com.Bertazz1.demo_park_api.web.dto.mapper.UserMapper;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -21,13 +22,13 @@ public class UserController {
     private final UserService userService;
 
     @PostMapping
-    public ResponseEntity<UserResposeDto> createUser(@RequestBody UserCreateDto userCreateDto) {
+    public ResponseEntity<UserResposeDto> createUser(@Valid @RequestBody UserCreateDto userCreateDto) {
         User savedUser = userService.createUser(UserMapper.toUser(userCreateDto));
         return ResponseEntity.status(HttpStatus.CREATED).body(UserMapper.toDto(savedUser));
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<UserResposeDto> getById(@PathVariable Long id) {
+    public ResponseEntity<UserResposeDto> getById(@Valid @PathVariable Long id) {
         User savedUser = userService.findById(id);
         return ResponseEntity.ok(UserMapper.toDto(savedUser));
     }
@@ -39,7 +40,7 @@ public class UserController {
     }
 
     @PatchMapping("/{id}")
-    public ResponseEntity<Void> updatePassword(@PathVariable Long id,@RequestBody UserPasswordDtio dto) {
+    public ResponseEntity<Void> updatePassword(@Valid @PathVariable Long id,@RequestBody UserPasswordDtio dto) {
         User savedUser = userService.updatePassword(id, dto.getOldPassword(), dto.getNewPassword(), dto.getConfirmNewPassword());
         return ResponseEntity.noContent().build();
     }
--- src/main/java/com/Bertazz1/demo_park_api/web/dto/UserPasswordDtio.java
+++ src/main/java/com/Bertazz1/demo_park_api/web/dto/UserPasswordDtio.java
@@ -1,6 +1,8 @@
 package com.Bertazz1.demo_park_api.web.dto;
 
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.*;
 
 @Getter
@@ -10,7 +12,15 @@ import lombok.*;
 @ToString
 public class UserPasswordDtio {
 
+    @NotBlank(message = "Password cannot be blank")
+    @Size(min = 6, max = 6, message = "Password must be exactly 6 characters long")
     private String oldPassword;
+
+    @NotBlank(message = "Password cannot be blank")
+    @Size(min = 6, max = 6, message = "Password must be exactly 6 characters long")
     private String newPassword;
+
+    @NotBlank(message = "Password cannot be blank")
+    @Size(min = 6, max = 6, message = "Password must be exactly 6 characters long")
     private String confirmNewPassword;
 }
